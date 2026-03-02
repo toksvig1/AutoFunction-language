5008c1d05b4c047fff87b403aad817aa354c3f49
toksvig <htkdavid@gmail.com>
2026-03-02 09:42:09 +0100

    tilføjer while

--- interpreter.py
+++ interpreter.py
@@ -40,7 +40,7 @@ class Inter:
         self.varsNoSF = {}
         lines = [x for x in s.split("\n") if x.strip() != ""]
         pc = 0
-        self.recurvline_func(lines,pc)
+        self.recurvline_func(lines,pc,0,False,False)
         print(self.varsNoSF)
     def ev_expr(self, s):
         toks = s.split()
@@ -67,35 +67,48 @@ class Inter:
                         stack = stack + [1]
                     else:
                         stack = stack + [0]
+                elif tok == "<":
+                    if lhs in self.varsNoSF: lhs = self.varsNoSF[lhs]
+                    if rhs in self.varsNoSF: rhs = self.varsNoSF[rhs]
+                    if lhs < rhs:
+                        stack = stack + [1]
+                    else:
+                        stack = stack + [0]
         return stack[0]
     
-    def recurvline_func(self,lines,pc):
+    def recurvline_func(self,lines,pc,loop,orgpos,exprorg):
         line = lines[pc]
         match line.split(maxsplit=1)[0]:
             case 'if':
                 if self.ev_expr(line.split(maxsplit=1)[1])==1: 
                     pc =pc+1 
-                    print("a")
                 else:
                     while lines[pc].split(maxsplit=1)[0] != "end": pc = pc + 1
                     pc = pc + 1
             case 'end': 
-                pc = pc + 1
+                if loop:
+                    if self.ev_expr(exprorg)==1:
+                        pc = int(orgpos)
+                    else:
+                        pc = pc + 1
+                else:
+                    pc = pc + 1
             case 'while':
                 print("a")
-                self.recurv_func(lines,pc)
+                if self.ev_expr(line.split(maxsplit=1)[1])==1: 
+                    self.recurvline_func(lines,pc,pc,True,line.split(maxsplit=1)[1])
             case _:
                 (name,_,expr) = line.split(maxsplit=2)
                 self.varsNoSF[name] = self.ev_expr(expr)
                 #print(name+" | "+expr)
                 pc = pc + 1
         if pc < len(lines):
-            self.recurvline_func(lines,pc)
+            self.recurvline_func(lines,pc,0,False,False)
 
-
-    def recurv_func(self, lines, pc):
+    """
+    def recurv_func(self, lines, pc, orgpos):
         print("run")
         if pc == 2:
             self.recurv_func(lines,pc)
-    
+    """
 Inter().ev(open(sys.argv[1]).read())
